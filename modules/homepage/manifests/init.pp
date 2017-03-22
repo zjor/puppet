a88ee4c3e6fe09c1cbecf62794d0fd74b58b40e5
sroyz <sroyz@cisco.com>
2017-03-22 17:50:10 +0100

    static file moved

--- modules/homepage/manifests/init.pp
+++ modules/homepage/manifests/init.pp
@@ -1,6 +1,10 @@
 class homepage {
 
-	file {'/var/www/homepage/index.html':
+	file { '/var/www/homepage':
+		ensure	=> directory
+	}
+
+	file { '/var/www/homepage/index.html':
 		ensure	=> file,
 		source	=> 'puppet:///modules/homepage/index.html'	
 	}
