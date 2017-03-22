e0f718a0b95097c3a7bd52c0c17c102dbb6a5b96
sroyz <sroyz@cisco.com>
2017-03-22 17:22:26 +0100

    template was added

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -2,4 +2,9 @@ class nginx {
   package { 'nginx':
     ensure => installed
   }
+
+  file { '/etc/nginx/conf.d/homepage.conf':
+  	ensure	=> file, 
+  	content	=> template('nginx/homepage.conf.erb')
+  }
 }
