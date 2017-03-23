324121cab0fb9dcc94dab379bc25048b94cd0e47
sroyz <sroyz@cisco.com>
2017-03-23 10:22:29 +0100

    index

--- modules/homepage/manifests/init.pp
+++ modules/homepage/manifests/init.pp
@@ -6,7 +6,7 @@ class homepage {
 		recurse	=> true
 	}
 
-	file { '/var/www/homepage/index.html':
+	file { '/www/homepage/index.html':
 		ensure	=> file,
 		source	=> 'puppet:///modules/homepage/index.html'		
 	}
