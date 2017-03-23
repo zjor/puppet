4578b6b60e86b7b3d3324cba1691bc74a750508c
sroyz <sroyz@cisco.com>
2017-03-23 09:58:36 +0100

    static page config

--- modules/homepage/manifests/init.pp
+++ modules/homepage/manifests/init.pp
@@ -1,12 +1,14 @@
 class homepage {
 
-	file { ['/var/www/', '/var/www/homepage']:
-		ensure	=> directory
+	file { ['/www/', '/www/homepage']:
+		ensure	=> directory,
+		owner	=> 'www-data',
+		recurse	=> true
 	}
 
 	file { '/var/www/homepage/index.html':
 		ensure	=> file,
-		source	=> 'puppet:///modules/homepage/index.html'	
+		source	=> 'puppet:///modules/homepage/index.html'		
 	}
 
 }
